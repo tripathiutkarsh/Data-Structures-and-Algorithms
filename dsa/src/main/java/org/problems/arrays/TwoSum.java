8948cbd59d491cd7624f911786c63d9a99568bf4
tripathiutkarsh <tripathiutkarsh@outlook.com>
2022-07-11 22:42:25 -0700

    slight improvement in 2-sum using Array

--- dsa/src/main/java/org/problems/arrays/TwoSum.java
+++ dsa/src/main/java/org/problems/arrays/TwoSum.java
@@ -6,47 +6,51 @@ import java.util.Map;
 
 public class TwoSum {
 
-    public static int[] twoSumNaive(int target, int[] nums){
+    public static int[] twoSumNaive(int target, int[] nums) {
         int[] result = new int[2];
-        for (int i=0; i< nums.length;i++)
-            for (int j=i+1; j< nums.length;j++)
-                if (nums[i] == target - nums[j]){
+        for (int i = 0; i < nums.length; i++)
+            for (int j = i + 1; j < nums.length; j++)
+                if (nums[i] == target - nums[j]) {
                     result[0] = i;
                     result[1] = j;
                 }
         return result;
     }
 
-    public static int[] twoSumUsingMap(int target, int[] nums){
+    public static int[] twoSumUsingMap(int target, int[] nums) {
         int[] result = new int[2];
         Map<Integer, Integer> vault = new HashMap<Integer, Integer>();
-        for (int i=0; i<nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             if (!vault.containsKey(nums[i]))
-                vault.put(target-nums[i], i);
+                vault.put(target - nums[i], i);
             else {
                 result[0] = vault.get(nums[i]);
                 result[1] = i;
             }
-            }
+        }
         return result;
     }
-    public static int[] twoSumUsingArray(int target, int[] nums){
+    // Work in Progress (Not all Tests are passing)
+    public static int[] twoSumUsingArray(int target, int[] nums) {
         int[] result = new int[2];
 
         // Calculate maximum element
         int max = Integer.MIN_VALUE;
-        for (int i=0; i< nums.length;i++)
+        for (int i = 0; i < nums.length; i++)
             max = nums[i] > max ? nums[i] : max;
-
-        int[] vault = new int[max+1];
+        // target might still be bigger than max
+        if (target > max)
+            max = target;
+        int[] vault = new int[max + 1];
         Arrays.fill(vault, Integer.MIN_VALUE);
-        for (int i=0; i<nums.length; i++){
-            if (vault[nums[i]]==Integer.MIN_VALUE)
-                vault[Math.abs(target-nums[i])] = i;
+        for (int i = 0; i < nums.length; i++) {
+            if (vault[nums[i]] == Integer.MIN_VALUE)
+                vault[Math.abs(target - nums[i])] = i; // Using absolute value in order to avoid -ve index and thus ArrayIndexOutOfBoundsException
             else {
                 result[0] = vault[nums[i]];
                 result[1] = i;
             }
         }
         return result;
-    }}
+    }
+}
